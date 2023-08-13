7b827477601334ef282fd18d2977c7c9b78c2671
PrincessChuks <princesschuks07@gmail.com>
2023-08-12 20:12:17 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: this is always 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a work of art,\n");
+	return (0);
+}
